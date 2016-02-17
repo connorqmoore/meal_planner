bd6ef08b3c1bd3afd3d49c11e40abba9c046161c
Connor Moore <cqm@andrew.cmu.edu>
2016-02-16 19:02:36 -0500

    JSON of food

--- gen/gen_food.py
+++ gen/gen_food.py
@@ -1,1 +1,19 @@
-# Work with Anthony to get a list of all meals
+import json
+import csv
+
+with open('menu_items.csv') as meal_csv:
+	# read csv
+	meal_reader = csv.reader(meal_csv, delimiter=',')
+	meal_reader = (list) (meal_reader)
+	# delete header
+	meal_reader = meal_reader[1:]
+
+	# make dict
+	meal_dict = {}
+	for item in meal_reader:
+		meal_dict[item[0]] = (item[1],item[2])
+	meals = json.dumps(meal_dict)
+
+# write json
+with open('menu_items.json', 'w') as meal_json:
+	json.dump(meal_dict, meal_json, sort_keys = True, indent = 4)
