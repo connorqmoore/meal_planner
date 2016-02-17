09c093f14e5f6143554a2f6edbfc713b6e9cb2a0
Connor Moore <cqm@andrew.cmu.edu>
2016-02-16 19:28:27 -0500

    refactor, added lunch and dinner parsing json

--- gen/gen_food.py
+++ gen/gen_food.py
@@ -1,7 +1,9 @@
+# python3
+
 import json
 import csv
 
-with open('menu_items.csv') as meal_csv:
+with open('items/menu_items.csv') as meal_csv:
 	# read csv
 	meal_reader = csv.reader(meal_csv, delimiter=',')
 	meal_reader = (list) (meal_reader)
@@ -12,8 +14,18 @@ with open('menu_items.csv') as meal_csv:
 	meal_dict = {}
 	for item in meal_reader:
 		meal_dict[item[0]] = (item[1],item[2])
-	meals = json.dumps(meal_dict)
 
 # write json
-with open('menu_items.json', 'w') as meal_json:
+with open('items/menu_items.json', 'w') as meal_json:
 	json.dump(meal_dict, meal_json, sort_keys = True, indent = 4)
+
+# write lunch meals
+with open('items/lunch_items.json', 'w') as meal_json:
+	lunch_dict = { item: meal_dict[item] for item in meal_dict.keys() if meal_dict[item][0] == "Lunch"}
+	json.dump(lunch_dict, meal_json, sort_keys = True, indent = 4)
+
+
+# write dinner meals
+with open('items/dinner_items.json', 'w') as meal_json:
+	dinner_dict = { item: meal_dict[item] for item in meal_dict.keys() if meal_dict[item][0] == "Dinner"}
+	json.dump(dinner_dict, meal_json, sort_keys = True, indent = 4)
